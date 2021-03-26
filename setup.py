a4ba482b08bdf70d2a55bce47a2ad257cd2944ac
Takamasa Iijima <takahyon@gmail.com>
2021-03-26 13:10:36 +0900

    fix setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="doktor-health",
+    name="doktor_health",
     version="0.1.0",
     author="Takamasa Iijima",
     author_email="takahyon@gmail.com",
